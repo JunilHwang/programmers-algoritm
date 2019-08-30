3e7166015c304730ba8d6378d62dc72aa7a8a396
junil-hwang <tjsdlf4261@naver.com>
2019-08-30 18:42:53 +0900

    힙 > 라면공장 - 효율성 통과

--- heep/2.py
+++ heep/2.py
@@ -4,17 +4,26 @@
 # 해당 시점에 공급 가능한 밀가루 수량(supplies)
 # 원래 공장으로부터 공급받을 수 있는 시점 k
 # 밀가루가 떨어지지 않고 공장을 운영하기 위해서 최소한 몇 번 해외 공장으로부터 밀가루를 공급받아야 하는지
-import heapq
+from heapq import heappush as push, heappop as pop
 
 def solution(stock, d, s, k):
-    n = 0
-    prev = 1
+    cnt = 0
+    i = 0
     h = []
-    for i in range(len(s)) : heapq.heappush(h, (-s[i], d[i]))
-    for i in range(len(s)) :
-        pop = heapq.heappop(h)
-        stock -= pop[0]
-        print(pop[1], stock, k)
-    return 0
+    for now in d:
+        push(h, -s[i])
+        if stock - now <= 0 or stock - h[0] >= k-1:
+            stock += -pop(h)
+            cnt += 1
+            if stock >= k-1 : break
+        i += 1
+    return cnt
 
-print(solution(5, [5, 4, 10, 12], [1, 1, 9, 1], 14))
+print(solution(4, [5, 6, 7, 12], [4, 5, 9, 1], 10))
+# 1 4
+# 2 3
+# 3 2
+# 4 1
+# 5 4
+# 6 8
+# 7 16
