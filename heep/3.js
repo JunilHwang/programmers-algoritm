061d8a6f50f8f2c2f915fda9ccfb23d92e4d3df9
junil-hwang <tjsdlf4261@naver.com>
2019-08-31 00:59:36 +0900

    힙 > 디스크 컨트롤러 - 정확성 통과

--- heep/3.js
+++ heep/3.js
@@ -1,21 +1,35 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42627?language=javascript
 
-function solution(jobs) {
-  let t = 0, now = jobs.shift()
-  const complete = [], q = []
+function solution(job) {
+  let t = 0, now = null, next = null
+  job.sort((a, b) => a[0] > b[0])
+  let complete = 0, q = [], total = job.length
   while (true) {
+    while (job.length && t === job[0][0]) {
+      q.push(job.shift())
+      q.sort((a, b) => a[1] > b[1])
+    }
+    if (now !== null) {
+      now[1] -= 1
+      if (now[1] === 0) {
+        complete += t - now[0]
+        now = null
+        if (job.length + q.length === 0) break
+      }
+    }
+    //console.log(t, now, q)
+    if (q[0] && now === null && q[0][0] <= t) {
+      now = q.shift()
+    }
     t++
-    now[1] -= 1
-    if (jobs[0] && t === jobs[0][0]) {
-      q.push(jobs.shift())
-    }
-    if (now[1] === 0) {
-      complete.push(t - now[0])
-      if (jobs.length + q.length === 0) break
-      now = q.sort((a, b) => a[1] > b[1]).shift()
-    }
   }
-  return ~~(complete.reduce((x, y) => x + y) / 3)
+  //console.log(complete)
+  return ~~(complete / total)
 }
 
+console.log(solution([[0, 9], [0, 4], [0, 5], [0, 7], [0, 3]]))
+console.log(solution([[15,34], [15,2]]))
+console.log(solution([[0, 5], [1, 2], [5, 5]]))
+console.log(solution([[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 2], [15, 34], [35, 43], [26, 1]]))
 console.log(solution([[0, 3], [1, 9], [2, 6]]))
+console.log(solution([[10, 6], [12, 6], [14, 4]]))
