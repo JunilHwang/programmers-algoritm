1935d1735edcbb10ee303a747be89bac458aa5e7
junil-hwang <tjsdlf4261@naver.com>
2019-08-30 23:33:39 +0900

    힙 > 라면공장 - 완료

--- heep/2-3.py
+++ heep/2-3.py
@@ -4,21 +4,19 @@
 # 해당 시점에 공급 가능한 밀가루 수량(supplies)
 # 원래 공장으로부터 공급받을 수 있는 시점 k
 # 밀가루가 떨어지지 않고 공장을 운영하기 위해서 최소한 몇 번 해외 공장으로부터 밀가루를 공급받아야 하는지
+
 from heapq import heappush as push, heappop as pop
-
 def solution(stock, d, s, k):
-    cnt = 0
-    i = 0
-    last = len(d)
     h = []
-    for now in range(k):
-        if i < last and now == d[i]:
+    last = len(d)
+    cnt = start = 0
+    while stock < k :
+        for i in range(start, last) :
+            if d[i] > stock : break
             push(h, -s[i])
-            i+=1
-        if not stock or (h and stock - h[0] >= k - 1):
-            stock = -pop(h)
-            cnt += 1
-            if stock >= k - now : return cnt
-        stock -= 1
-
+            start = i + 1
+        stock -= pop(h)
+        cnt += 1
+    return cnt
+  
 print(solution(4, [4, 10, 15], [20, 5, 10], 30))
--- heep/2.py
+++ heep/2.py
@@ -6,21 +6,21 @@
 # 밀가루가 떨어지지 않고 공장을 운영하기 위해서 최소한 몇 번 해외 공장으로부터 밀가루를 공급받아야 하는지
 from heapq import heappush as push, heappop as pop
 
-# 테스트 1 〉	통과 (3.05ms, 22.2MB)
-# 테스트 2 〉	통과 (0.22ms, 18.3MB)
-# 테스트 3 〉	통과 (1.01ms, 11.7MB)
+# 테스트 1 〉	통과 (2.59ms, 22.3MB)
+# 테스트 2 〉	통과 (0.54ms, 18.3MB)
+# 테스트 3 〉	통과 (0.85ms, 11.8MB)
 def solution(stock, d, s, k):
-    cnt = 0
-    i = 0
     h = []
-    for now in d:
+    d += [k]
+    s += [0]
+    cnt = i = 0
+    for now in d :
         push(h, -s[i])
-        if stock - now <= 0 or stock - h[0] >= k-1:
-            print(now)
-            stock += -pop(h)
-            cnt += 1
-            if stock >= k-1 : return cnt
         i += 1
+        while stock <= now :
+            stock -= pop(h)
+            cnt += 1
+            if stock >= k : return cnt
 
 # 테스트 1 〉	통과 (2.99ms, 22.3MB)
 # 테스트 2 〉	통과 (0.63ms, 18.1MB)
