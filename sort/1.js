14688c65b6b54c8254fce9033d44fe46a0efff32
junil-hwang <tjsdlf4261@naver.com>
2019-08-31 15:34:53 +0900

    정렬>K번째수 - 리팩토링

--- sort/1.js
+++ sort/1.js
@@ -1,10 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42748?language=javascript
 function solution(array, commands) {
-  var answer = [];
-  for (const [i, j, k] of commands) {
-    answer.push(array.slice(i-1, j).sort((a, b) => a- b)[k-1])
-  }
-  return answer;
+  return commands.map(([i, j, k]) => array.slice(i-1, j).sort((a, b) => a- b)[k-1])
 }
 
 console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]),[5, 6, 3])
