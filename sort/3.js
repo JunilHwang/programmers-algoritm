540b94903f5ed20e96c537323fa0a92f78a16c65
junil-hwang <tjsdlf4261@naver.com>
2019-08-31 16:42:05 +0900

    정렬 > H-index - 리팩토링

--- sort/3.js
+++ sort/3.js
@@ -1,10 +1,7 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42747?language=javascript
 function solution(citations) {
-  const len = citations
-  const max = Math.max(...citations)
-  for (let h = max; h > -1 ; h--) {
+  for (let h = Math.max(...citations); h > -1 ; h--)
     if (citations.filter(v => v >= h).length >= h) return h
-  }
   return 0
 }
 
