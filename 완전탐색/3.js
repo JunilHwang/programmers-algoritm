9ba44c5e38056eca49376a7d2634783d62071d41
junil-hwang <tjsdlf4261@naver.com>
2019-09-02 01:57:34 +0900

    완전탐색 > 숫자 야구 - 채점중

--- 완전탐색/3.js
+++ 완전탐색/3.js
@@ -1,19 +1,39 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42841
 
 function solution(baseball) {
-  const arr = baseball.filter(([x, y, z]) => y + z > 0)
+  const arr = baseball.filter(([x, y, z]) => {
+      const chk1 = y + z > 0
+      const chk2 = `${x}`.indexOf('0') === -1
+      return chk1 && chk2
+  })
   const len = arr.length
   const stack = []
   let answer = 0
   const f = ([a1, a2, a3], b, k) => {
-    if (k === arr.length) {
+    if (k >= len) {
       (a1.length ? a1 : b).forEach(v1 => {          
         (a2.length ? a2 : b).forEach(v2 => {
           (a3.length ? a3 : b).forEach(v3 => {
-            const n = `${v1}${v2}${v3}`*1
-            if (stack.indexOf(n) !== -1) return
-            f2(n)
-            stack.push(n)
+            const nums = [`${v1}${v2}${v3}`*1]
+            const blen = b.length
+            if (blen >= 1) {
+              b.forEach(b1 => nums.push(`${b1}${v2}${v3}`*1, `${v1}${b1}${v3}`*1, `${v1}${v2}${b1}`*1))
+            }
+            if (blen >= 2) {
+              b.forEach(b1 => b.forEach(b2 => {
+                if (b1 !== b2) nums.push(`${b1}${b2}${v3}`*1, `${b1}${v2}${b2}`*1, `${v1}${b1}${b2}`*1)
+              }))
+            }
+            if (blen === 3) {
+              b.forEach(b1 => b.forEach(b2 => b.forEach(b3 => {
+                if (b1 !== b2 && b1 !== b3 && b2 === b3) nums.push(`${b1}${b2}${b3}`*1)
+              })))
+            }
+            nums.forEach(n => {
+              if (stack.indexOf(n) !== -1) return
+              f2(n)
+              stack.push(n)
+            })
           })
         })
       })
