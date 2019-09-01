e1ed880764af5a70f488f16d58dac2e28c9b6ae4
junil-hwang <tjsdlf4261@naver.com>
2019-09-02 03:21:03 +0900

    완전탐색 > 숫자 야구 - 완료

--- 완전탐색/3.js
+++ 완전탐색/3.js
@@ -2,15 +2,29 @@
 
 function solution(baseball) {
   let answer = 0
-  const nums = [1,2,3,4,5,6,7,8,9]
+  let nums = [1,2,3,4,5,6,7,8,9]
+  const ball = baseball.filter(v => {
+    const x = Array.from(`${v[0]}`).map(v => v*1)
+    if (x.indexOf(0) !== -1 || x.length !== 3) return false
+    if (v[1] + v[2] === 0) {
+      nums = nums.filter(n => x.indexOf(n) === -1)
+      return false
+    }
+    return true
+  })
   const chk = nArr => {
-    for (const [x, y, z] of baseball) {
-      const xArr = Array.from(x+'').map(v => v*1)
+    for (const [x, y, z] of ball) {
       let yChk = 0, zChk = 0
-      for (let i = 0; i < 3; i++) if (xArr[i] === nArr[i]) yChk++;
+      const xArr = Array.from(x+'').map(v => v*1)
+      if (xArr[0] === nArr[0]) yChk++;
+      if (xArr[1] === nArr[1]) yChk++;
+      if (xArr[2] === nArr[2]) yChk++;
       if (yChk !== y) return
-      for (let i = 0; i < 3; i++) if (xArr.indexOf(nArr[i]) !== -1) zChk++;
-      if (zChk < z) return
+      if (nArr[0] === xArr[1] || nArr[0] === xArr[2]) zChk++
+      if (nArr[1] === xArr[0] || nArr[1] === xArr[2]) zChk++
+      if (nArr[2] === xArr[1] || nArr[2] === xArr[0]) zChk++
+      if (zChk !== z) return
+      //console.log(nArr, x, y, yChk, z, zChk)
     }
     answer++
   }
@@ -26,3 +40,4 @@ function solution(baseball) {
 }
 
 console.log(solution([[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]), 2)
+//console.log(solution([[123, 0, 0], [456, 3, 0]]), 1)
