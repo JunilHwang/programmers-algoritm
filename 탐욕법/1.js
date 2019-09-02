51957dde0cb4161ec846756492abc7eb15a8d043
JunilHwang <tjsdlf4261@naver.com>
2019-09-02 14:03:52 +0900

    탐욕법 > 체육복 - 완료

--- 탐욕법/1.js
+++ 탐욕법/1.js
@@ -4,7 +4,7 @@ function solution(n, lost, reserve) {
   for (let i = 1; i <= n; i++) arr[i] = 1
   lost.forEach(v => arr[v] -= 1)
   reserve.forEach(v => arr[v] += 1)
-  for (let i = 1; i < n; i++) {
+  for (let i = 1; i <= n; i++) {
     if (arr[i] === 2 && arr[i - 1] === 0) arr[i - 1] = arr[i] = 1
     if (arr[i] === 2 && arr[i + 1] === 0) arr[i + 1] = arr[i] = 1
   }
@@ -14,3 +14,4 @@ function solution(n, lost, reserve) {
 console.log(solution(5, [2, 4], [1, 3, 5]), 5)
 console.log(solution(5, [2, 4], [3]), 4)
 console.log(solution(3, [3], [1]), 2)
+console.log(solution(2, [1], [2]), 2)
