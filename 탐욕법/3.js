818c1a458bf269e643bfc5fbdd48833b7c1dc6b2
junil-hwang <tjsdlf4261@naver.com>
2019-09-03 00:29:01 +0900

    탐욕법 > 큰 수 만들기 - 1개 실패

--- 탐욕법/3.js
+++ 탐욕법/3.js
@@ -2,31 +2,19 @@
 
 function solution (nums, k) {
   const last = nums.length, len = last - k
-  let left = 0, n = nums.substr(left, len), max = n, maxLeft = 0
-  do {
-    if (max < n) {
-      max = n
-      maxLeft = left
-    }
-    n = nums.substr(++left, len)
-  } while (n.length === len)
-  let num = max[0]
-  for (let i = 1; i < len; i++) {
-    left = maxLeft = maxLeft+1
+  let left = 0, n = nums.substr(left, len), max = n, maxLeft = 0, num = ""
+  for (let i = 0; i < len; i++) {
     max = n = nums.substr(left, len - i)
     do {
-      if (max < n) {
-        max = n
-        maxLeft = left
-      }
+      if (max[0] < n[0]) max = n, maxLeft = left
       n = nums.substr(++left, len - i)
     } while (n.length === len - i)
     num += max[0]
+    left = ++maxLeft
   }
   return num
 }
 
-//console.log(solution("1924", 2), "94")
-//console.log(solution("1231234", 3), "3234")
+console.log(solution("1924", 2), "94")
+console.log(solution("1231234", 3), "3234")
 console.log(solution("4177252841", 4), "775841")
-72528
