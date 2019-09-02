6ad944bfa225bc31981f1f8aba321e894316f91c
JunilHwang <tjsdlf4261@naver.com>
2019-09-02 15:26:43 +0900

    탐욕법 > 조이스틱 - 완료

--- 탐욕법/2.js
+++ 탐욕법/2.js
@@ -8,26 +8,26 @@ function solution(name) {
     now.push(0), last.push(code), rmd.push(!code)
   })
   let min = Infinity
-  const f = (n, p, nowTmp, rmdTmp, tmp1 = last[p], tmp2 = zCode - tmp1 + 1) => {
+  const f = (n, p, now, rmd, tmp1 = last[p], tmp2 = zCode - tmp1 + 1) => {
     if ( n > min ) return
-    if (tmp1 !== nowTmp[p]) {
+    if (tmp1 !== now[p]) {
       const tmp = tmp1 < tmp2 ? tmp1 : tmp2
-      nowTmp[p] = tmp1, rmdTmp[p] = true, n += tmp
+      now[p] = tmp1, rmd[p] = true, n += tmp
     }
-    if (rmdTmp.find(v => !v) === undefined) { min = n; return; }
+    if (rmd.find(v => !v) === undefined && n < min) { min = n; return; }
     for (let i = 1, j = p + 1; i < len; i++, j++) {
       if (j >= len) j = 0
-      if (!rmdTmp[j]) {f(n + i, j, [...nowTmp], [...rmdTmp]); break;}
+      if (!rmd[j]) {f(n + i, j, [...now], [...rmd]); break;}
     }
     for (let i = 1, j = p - 1; i < len; i++, j--) {
       if (j < 0) j = len - 1
-      if (!rmdTmp[j]) { f(n + i, j, [...nowTmp], [...rmdTmp]); break; }
+      if (!rmd[j]) { f(n + i, j, [...now], [...rmd]); break; }
     }
   }
   f(0, 0, [...now], [...rmd])
   return min;
 }
 
-console.log(solution("JEROEN"), 56)
-console.log(solution("JAN"), 23)
+//console.log(solution("JEROEN"), 56)
+//console.log(solution("JAN"), 23)
 console.log(solution("AAABAAA"), 0)
