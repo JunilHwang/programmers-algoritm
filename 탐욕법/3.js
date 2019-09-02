ca7ec0ee6daefb5deb4e0e65de9a4233fecb414e
JunilHwang <tjsdlf4261@naver.com>
2019-09-02 16:58:05 +0900

    탐욕법 > 큰 수 만들기 - 재작성

--- 탐욕법/3.js
+++ 탐욕법/3.js
@@ -1,22 +1,18 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42883?language=javascript
 
-function solution(number, k) {
-  const numbers = Array.from(number)
-  let max = 0
-  const f = (arr, n, num = arr.join('') * 1) => {
-    if (num < max) return
-    if (n === k && num > max) {
-      max = num
-      return
-    }
-    arr.forEach((v, k) => {
-      const temp = [...arr]
-      temp.splice(k, 1)
-      f(temp, n + 1)
-    })
+function solution (number, k) {
+  const nums = Array.from(number).map(v => v*1)
+  let n = 0, i = 0
+  while (n < k) {
+    if (nums[i] === nums[i + 1]) { i++; continue; }
+    if (nums[i] > nums[i + 1]) {
+      const n1 = `${nums[i]}${nums[i+1]}`*1
+      const n2 = `${nums[i]}${nums[i+2]}`*1
+      nums.splice(n1 < n2 ? i+1 : i+2, 1)
+    } else nums.splice(0, 1)
+    n++
   }
-  f([...numbers], 0)
-  return max + '';
+  return nums.join('')
 }
 
 console.log(solution("1924", 2), "94")
