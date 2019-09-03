200ce2baeb0e212ed096fdee3edfbf9388b25b74
JunilHwang <tjsdlf4261@naver.com>
2019-09-03 12:51:26 +0900

    탐욕법 > 구명보트 - 재작성 (2)

--- 탐욕법/4.js
+++ 탐욕법/4.js
@@ -1,16 +1,28 @@
 // 구명보트 : https://programmers.co.kr/learn/courses/30/lessons/42885?language=javascript
 function solution(people, limit) {
+  let cnt = 0, sum = 0
   const len = people.length
-  let cnt = 1, sum = 0
-  people.sort((a, b) => a - b)
+  const chk = people.map(v => 0)
   for (let i = 0; i < len; i++) {
-    sum += people[i]
-    if (sum > limit) {
-      sum = people[i], cnt += 1
+    if(chk[i]) continue
+    const n = people[i]
+    const idx = people.findIndex((v, k) => !chk[k] && i != k && n+v === limit)
+    if (idx !== -1) {
+      chk[idx] = true, cnt += 1
+      continue
     }
+    let sum = n
+    while (1) {
+      let idx = people.findIndex((v, k) => !chk[k] && i != k && sum+v <= limit)
+      if (idx === -1) break
+      chk[idx] = true
+      sum += people[idx]
+    }
+    cnt += 1
   }
   return cnt;
 }
 
-console.log(solution([70, 50, 80, 50], 100), 	3)
-console.log(solution([70, 80, 50], 100), 	3)
+//console.log(solution([70, 50, 80, 50], 100), 	3)
+//console.log(solution([70, 80, 50], 100), 	3)
+console.log(solution([50, 40, 50, 60, 70, 10, 10, 10], 100), 3)
