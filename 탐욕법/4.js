98d4b0578fef7f352b5cea9ad95b16a2a7401af1
JunilHwang <tjsdlf4261@naver.com>
2019-09-05 14:06:23 +0900

    탐용법 > 구명보트 - 완료

--- 탐욕법/4.js
+++ 탐욕법/4.js
@@ -2,11 +2,11 @@
 function solution(people, limit) {
   const nums = people.sort((a, b) => a - b)
   const len = nums.length, chk = nums.map(v => 0) 
-  let cnt = 0
+  let cnt = 0, j = len - 1
   loop: for (let i = 0; i < len; i++) {
     if (chk[i]) continue
     chk[i] = 1
-    for (let j = len - i; j > i; j--) {
+    for (; j > i; j--) {
       if (chk[j]) continue
       if (nums[i] + nums[j] === limit) {
         cnt += 1, chk[j] = 1; continue loop
