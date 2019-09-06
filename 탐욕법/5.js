18fda73640c46eec2ae33eba03edb2b2bcc22494
JunilHwang <tjsdlf4261@naver.com>
2019-09-06 20:05:19 +0900

    탐역법 > 섬 연결하기 - 코드수정

--- 탐욕법/5.js
+++ 탐욕법/5.js
@@ -1,32 +1,26 @@
 // 섬연결 : https://programmers.co.kr/learn/courses/30/lessons/42861?language=javascript
 function solution(n, costs) {
-  const node = []
-  costs.forEach(([x, y]) => {
-    if (node.indexOf(x) === -1) node.push(x)
-    if (node.indexOf(y) === -1) node.push(y)
-  })
-  const path = node.map(x => node.map(y => x===y ? 0 : -1))
-  costs.forEach(([x, y, t]) => {
-    path[x][y] = t
-    path[y][x] = t
-  })  
-  let min = Infinity
-  const f = (p, n, len) => {
-    if (n > min) return
-    if (len === node.length) {
-      if (n < min) min = n
-      return
-    }
-    node.forEach(to => {
-      if (p.indexOf(to) !== -1) return
-      const from = p[len - 1]
-      const add = path[from] === undefined ? 0 : path[from][to]
-      if (add === -1) return
-      f([...p, to], n + add, len + 1)
+  costs.sort((a, b) => a[0] - b[0])
+  const node = [...Array(n).keys()]
+  const min = node.map(v => [])
+  const path = node.map(x => node.map(y => x === y ? 0 : -1))
+  const sum = arr => arr[1] === undefined ? arr[0] : arr.reduce((x, y) => x + y)
+  costs.forEach(([x, y, t]) => { path[x][y] = path[y][x] = t })
+  const f = (k, t, count) => {
+    if (count >= n || sum(min[k]) < sum(t)) return
+    min[k] = t
+    path[k].forEach((v, k2) => {
+      if (v < 1 || k2 === k || k2 === 0) return
+      f(k2, [...t, v], count + 1)
     })
   }
-  f([], 0, 0)
-  return min;
+  path[0].forEach((v, k) => {
+    if (v < 1) return
+    f(k, [v], 0)
+  })
+  let a = 0
+  for (let i = 1; i < n; i++) a += min[i].pop()
+  return a
 }
 
 console.log(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]), 4)
